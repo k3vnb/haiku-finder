d5dc9764cc7dd072e50309e15a2d614c92bde41c
Kevin Boyle <papershack@gmail.com>
2018-01-04 09:24:05 -0800

    set up initial file structure, install inpm & gulp

---
+++ js/haiku-interface.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('#text-entry').submit(function(event){
+    event.preventDefault();
+    console.log("hello");
+    var goal = $('#goal').val();
+    var output = pingPong(goal);
+    output.forEach(function(element) {
+  $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
